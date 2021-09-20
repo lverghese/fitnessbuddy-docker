102ce49012f0decfa92d13277a7bb8970f5ee050
Carol Luce <cluce059@gmail.com>
2021-09-20 19:46:30 -0400

    test

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 
 const {goals} = require('./db/testdata.json');
 
-
 app.get('/', (req, res) => {
+    //login page 
   res.send('FITNESS BUDDY');
 });
 
 app.get('api/goals', (req, res) => {
+    //from signup ->
     res.send('What are ur goals?');
 });
 
