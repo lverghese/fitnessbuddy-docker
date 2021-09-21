cc49de63232ba9369ea5c3fe8d489659668319c8
Conner Cochrane <cccochrane1@knights.ucf.edu>
2021-09-20 20:45:10 -0400

    started seeds

--- seeds/exercises-seeds.js
+++ seeds/exercises-seeds.js
@@ -1,3 +1,25 @@
 const {exercises} = require ('../models')
 
-const exercisesData = []
+const exercisesData = [
+    {
+        exercise_data: "day 1",
+        muscle: "back"
+    },
+    {
+        exercise_data: "day 2",
+        muscle: "shoulder"
+    },
+    {
+        exercise_data: "day 3",
+        muscle: "legs"
+    },
+    {
+        exercise_data: "day 4",
+        muscle: "arms"
+    },
+    {
+        exercise_data: "day 5",
+        muscle: "chest"
+    }
+
+]
