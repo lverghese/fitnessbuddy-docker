87fba234cac7d83fba4d2d08b782ca18c3c93643
lverghese <loganverghese@gmail.com>
2021-09-23 20:30:59 -0400

    association init

--- models/index.js
+++ models/index.js
@@ -4,3 +4,31 @@ const Day = require('./Day');
 const Bulk = require('./Bulk');
 const Cut = require('./Cut');
 const Tone = require('./Tone');
+
+
+Bulk.belongsTo(Plan, {
+    foreignKey: 'plan_id'
+})
+
+Cut.belongsTo(Plan, {
+    foreignKey: 'plan_id'
+})
+
+Tone.belongsTo(Plan, {
+    foreignKey: 'plan_id'
+})
+
+Bulk.belongsTo(Day, {
+    foreignKey: 'day_id'
+})
+
+Cut.belongsTo(Day, {
+    foreignKey: 'day_id'
+})
+
+Tone.belongsTo(Day, {
+    foreignKey: 'day_id'
+})
+
+
+
