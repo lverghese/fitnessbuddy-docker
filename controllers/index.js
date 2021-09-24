22669c703cac7fda7af820fa29f939533d08e2f1
Carol Luce <cluce059@gmail.com>
2021-09-23 20:12:48 -0400

    debgging server

--- controllers/index.js
+++ controllers/index.js
@@ -5,6 +5,11 @@ const dashboardRoutes = require('./dashboard-routes.js');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+//router.use('/dashboard', dashboardRoutes);
+
+router.use((req, res) => {
+    res.status(404).end();
+  });
 
 module.exports = router;
 
--- models/index.js
+++ models/index.js
@@ -4,3 +4,5 @@ const Day = require('./Day');
 const Bulk = require('./Bulk');
 const Cut = require('./Cut');
 const Tone = require('./Tone');
+
+//model associatios here
