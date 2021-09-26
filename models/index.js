1431ffb6a9e3c016b3ad3cb34091a4cb9ff17a08
Conner Cochrane <cccochrane1@knights.ucf.edu>
2021-09-26 14:42:43 -0400

    updating routes to remove files no longer needed

--- models/index.js
+++ models/index.js
@@ -1,9 +1,5 @@
 //importing Models
 const Plan = require('./Plan');
-const Day = require('./Day');
-// const Bulk = require('./Bulk');
-const Cut = require('./Cut');
-const Tone = require('./Tone');
 const User = require('./User');
 
 module.exports = { User };
--- server.js
+++ server.js
@@ -30,6 +30,8 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
