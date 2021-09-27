5ff889911643f4cebd594f6c13ba8941844b61f9
Carol Luce <cluce059@gmail.com>
2021-09-27 13:21:43 -0400

    pluralised

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,16 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes');
+
 
 
 router.use('/api', apiRoutes);
 
 //use localhost:3001 as home route
 router.use('/', homeRoutes);
-
+router.use('/dashboard', dashboardRoutes);
+router.use('/home', homeRoutes);
 router.use((req, res) => {
     res.status(404).end();
   });
