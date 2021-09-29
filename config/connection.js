d606324955efd73c13d43392fb6087ce2c85f168
Carol Luce <cluce059@gmail.com>
2021-09-29 18:36:51 -0400

    addheroku

--- config/connection.js
+++ config/connection.js
@@ -1,28 +1,29 @@
 // import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
-require('dotenv').config();
+require('dotenv').con
 
-// if (process.env.JAWSDB_URL) {
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-//   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     port: 3306
-//   });
-// };
-
-///hardcode .env for npm run seedsd to work
 let sequelize;
 
  if (process.env.JAWSDB_URL) {
    sequelize = new Sequelize(process.env.JAWSDB_URL);
- } else {
-     sequelize = new Sequelize('fitness_buddy_db', 'root', 'USCaptain33', {
-     host: 'localhost',
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
      dialect: 'mysql',
      port: 3306
    });
- };
+};
+
+///hardcode .env for npm run seedsd to work
+
+//  if (process.env.JAWSDB_URL) {
+//    sequelize = new Sequelize(process.env.JAWSDB_URL);
+//  } else {
+//      sequelize = new Sequelize('fitness_buddy_db', 'root', 'USCaptain33', {
+//      host: 'localhost',
+//      dialect: 'mysql',
+//      port: 3306
+//    });
+//  };
 
 module.exports = sequelize;
