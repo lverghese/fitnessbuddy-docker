4364cc1a92c7ca6810b8eb9bf68af6367a5f9925
lverghese <loganverghese@gmail.com>
2021-10-03 16:38:40 -0400

    no dotenv needed on server.js, rearranged for cleanliness

--- server.js
+++ server.js
@@ -3,14 +3,17 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
-require('dotenv').config();
+const app = express();
 // init handlebars for html templates
 const hbs = exphbs.create({});
+
+// init server
+const PORT = process.env.PORT || 3001;
+
+
+
 // Express session for session cookies
 const session = require('express-session');
-// init server
-const app = express();
-const PORT = process.env.PORT || 3001;
 // Sequelize store session
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -27,13 +30,15 @@ const sess = {
   };
 
   app.use(session(sess));
+  app.engine('handlebars', hbs.engine);
+  app.set('view engine', 'handlebars');
+  
 //express parses json and string data
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 // path to public directory for static files
   app.use(express.static(path.join(__dirname, 'public')));
-  app.engine('handlebars', hbs.engine);
-  app.set('view engine', 'handlebars');
+
   //path to routes for server
   app.use(routes);
 
