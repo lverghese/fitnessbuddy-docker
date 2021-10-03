f1485dbc3581b6219e271f288c184be160629373
lverghese <loganverghese@gmail.com>
2021-10-03 16:27:40 -0400

    on click refresh

--- config/connection.js
+++ config/connection.js
@@ -20,7 +20,7 @@ let sequelize;
   if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
   } else {
-      sequelize = new Sequelize('fitness_buddy_db', 'root', 'Cena2022!', {
+      sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
       port: 3306
