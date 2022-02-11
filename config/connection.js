5c2a879987e9c7f4888bf2f909d4d3058d9d90fb
lverghese <loganverghese@gmail.com>
2022-02-11 17:36:31 -0500

    clean

--- config/connection.js
+++ config/connection.js
@@ -5,16 +5,6 @@ require('dotenv').config();
 
 let sequelize;
 
-// if (process.env.JAWSDB_URL) {
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-//   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     port: 3306
-//   });
-// };
-
 ///hardcode .env for npm run seedsd to work
 
   if (process.env.JAWSDB_URL) {
